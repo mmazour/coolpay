34f49b380e756b61eee224243de5ed8bc3e8c70d
Michael Mazour <michaelmazour@gmail.com>
2019-03-20 03:13:05 +0000

    Generated by Bundler

---
+++ lib/mmazour_coolpay.rb
@@ -0,0 +1,6 @@
+require "mmazour_coolpay/version"
+
+module MmazourCoolpay
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mmazour_coolpay/version.rb
@@ -0,0 +1,3 @@
+module MmazourCoolpay
+  VERSION = "0.1.0"
+end
